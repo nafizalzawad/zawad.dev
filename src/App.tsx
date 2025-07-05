eb6533192cd1a96312662cb5b9438ccc4b880f20
RedFlagZawad <117999027+nafizalzawad@users.noreply.github.com>
2025-07-05 21:54:22 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import Index from "./pages/Index";
 import WritingsPage from "./pages/WritingsPage";
+import PremiumPortfolioPage from "./pages/PremiumPortfolioPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/writings" element={<WritingsPage />} />
+          <Route path="/premium-portfolio" element={<PremiumPortfolioPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
